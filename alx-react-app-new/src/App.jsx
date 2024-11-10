4630f1e64408d4784eab50c5a6c8309bcdd4fba3
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-11-10 22:38:06 +0000

    counter created

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -30,6 +31,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
