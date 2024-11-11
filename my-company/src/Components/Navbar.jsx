72bfcd5f0deef689ce6104e6aa8444e005448e5a
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-11-11 09:04:42 +0000

    styling changed for Navbar.jsx

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav style={{ backgroundColor: '#f0f0f0', padding: '10px' }}>
-      <ul style={{ listStyleType: 'none', padding: '0' }}>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>
+      <ul style={{ listStyleType: 'none', padding: '0', display: 'flex', justifyContent: 'center' }}>
+        <li style={{ marginRight: '20px' }}>
           <Link to="/" style={{ textDecoration: 'none', color: '#333' }}>Home</Link>
         </li>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>
+        <li style={{ marginRight: '20px' }}>
           <Link to="/About" style={{ textDecoration: 'none', color: '#333' }}>About</Link>
 
         </li>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>
+        <li style={{ marginRight: '20px' }}>
           <Link to="/services" style={{ textDecoration: 'none', color: '#333' }}>Services</Link>
         </li>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>
+        <li style={{ marginRight: '20px' }}>
           <Link to="/contact" style={{ textDecoration: 'none', color: '#333' }}>Contact</Link>
         </li>
       </ul>
