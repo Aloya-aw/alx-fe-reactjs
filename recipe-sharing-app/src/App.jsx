e8dacf880232ee898812fb2bd9602b3c9d97caa7
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-11-17 21:33:51 +0000

    new react app created recipe list

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p>
+        <AddRecipeForm />
+        <RecipeList />
+      </p>
+    </div>
+  )
+}
+
+export default App
