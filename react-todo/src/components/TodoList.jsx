986dc13351c610407c921dd69037aeb9f2ef3b78
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-11-24 23:33:16 +0000

    changes made

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,33 +1,21 @@
-// TodoList.js
 import React, { useState } from 'react';
 
-const TodoList = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build a Todo App', completed: false },
-  ]);
-
-  const handleAddTodo = (text) => {
-    const newTodo = { id: Date.now(), text, completed: false };
-    setTodos([...todos, newTodo]);
-  };
-
-  const handleToggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const handleDeleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+const TodoList = ({ todos, onToggle, onDelete }) => { // Assuming these are passed as props from a parent component
 
   return (
-    <div>
-      {/* ... (Add Todo Form, Todo List, etc.) */}
-    </div>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}> {/* Add the key prop here */}
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => onToggle(todo.id)} // Assuming onToggle is a prop function for handling toggles
+          />
+          <span>{todo.text}</span>
+          <button onClick={() => onDelete(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
