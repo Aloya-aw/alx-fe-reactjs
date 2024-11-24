daef8a6c288a14d7fe372c91d3fb445677b8f3d9
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-11-24 21:41:37 +0000

    changes made

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,65 +1,53 @@
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const FormikForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      email: '',
-      password: ''
-    },
-    validationSchema: Yup.object({
-      username: Yup.string().required('Username is required'),
-      email: Yup.string().required('Email is required'),
-      password: Yup.string().required('Password is required')
-    }),
-    onSubmit: values => {
-      console.log(values);
-    }
+  const initialValues = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Required'),
+    email: Yup.string().required('Required'),
+    password: Yup.string().required('Required')
   });
+
+  const onSubmit = values => {
+    console.log(values);
+  };
 
   return (
     <div>
-      <h2>Formik Registration</h2>
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            onChange={formik.handleChange}
-            value={formik.values.username}
-          />
-          {formik.errors.username && (
-            <span>{formik.errors.username}</span>
-          )}
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-          {formik.errors.email && (
-            <span>{formik.errors.email}</span>
-          )}
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-          {formik.errors.password && (
-            <span>{formik.errors.password}</span>
-          )}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <h2>Registration</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <div>
+            <label>Username</label>
+            <Field name="username" type="text" />
+            <ErrorMessage name="username" />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <Field name="email" type="email" />
+            <ErrorMessage name="email" />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <Field name="password" type="password" />
+            <ErrorMessage name="password" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
