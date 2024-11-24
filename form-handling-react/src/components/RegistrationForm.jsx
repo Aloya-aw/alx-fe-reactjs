8b7685a13b2321fa0a458a6212ebb952a4e9afca
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-11-24 20:45:42 +0000

    component adjusted

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,6 +4,25 @@ const RegistrationForm = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!username) {
+      errors.username = "Username is required";
+    }
+
+    if (!email) {
+      errors.email = "Email is required";
+    }
+
+    if (!password) {
+      errors.password = "Password is required";
+    }
+
+    setErrors(errors);
+  };
 
   return (
     <div>
@@ -12,14 +31,17 @@ const RegistrationForm = () => {
         <div>
           <label>Username:</label>
           <input type="text" value={username} />
+          {errors.username && <span>{errors.username}</span>}
         </div>
         <div>
           <label>Email:</label>
           <input type="email" value={email} />
+          {errors.email && <span>{errors.email}</span>}
         </div>
         <div>
           <label>Password:</label>
           <input type="password" value={password} />
+          {errors.password && <span>{errors.password}</span>}
         </div>
         <button type="submit">Submit</button>
       </form>
